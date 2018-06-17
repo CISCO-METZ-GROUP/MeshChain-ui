976bdf9b1e19da70cc7d4039380d52f176c2e4ff
Lubomir Balogh <lubalogh@cisco.com>
2018-06-17 15:59:18 +0200

    Fix smart-contract service

--- src/app/smart-contract/smart-contract.service.ts
+++ src/app/smart-contract/smart-contract.service.ts
@@ -4,6 +4,7 @@ import { AppConfig } from '../app-config';
 
 import { CoreService } from '../shared/core.service';
 import { NotificationService } from '../notification/notification.service';
+import { Subject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -11,6 +12,8 @@ import { NotificationService } from '../notification/notification.service';
 export class SmartContractService {
 
   public contractDeployed: boolean;
+  public contractDeployedSubject: Subject<boolean> = new Subject();
+  public smartContract;
 
   constructor(
     private http: HttpClient,
@@ -28,6 +31,7 @@ export class SmartContractService {
 
     this.http.post(AppConfig.REST_BASE_URL + '/create-contract', body).subscribe(
       res => {
+        this.smartContract = res;
         this.subscribe();
       },
       error => {
@@ -37,15 +41,40 @@ export class SmartContractService {
     );
   }
 
+  public getAddress(): string {
+    if (this.smartContract) {
+      return this.smartContract.receipts[1].vibes.contractAddress;
+    } else {
+      return '';
+    }
+  }
+
   private subscribe() {
     const body = {
-      address: AppConfig.ADDRESS
+      address: this.getAddress()
     };
 
     this.http.post(AppConfig.REST_BASE_URL + '/subscribe', body).subscribe(
       res => {
+        this.subscribeVerdict();
+      },
+      error => {
+        this.coreService.isWorkingSubject.next(false);
+        this.notificationService.openModal('Subcription Failed', 'Subscribe Function Has Failed', 'danger');
+      }
+    );
+  }
+
+  private subscribeVerdict() {
+    const body = {
+      address: this.getAddress()
+    };
+
+    this.http.post(AppConfig.REST_BASE_URL + '/subscribeVerdict', body).subscribe(
+      res => {
         this.coreService.isWorkingSubject.next(false);
         this.notificationService.openModal('Deployment Successful', 'Smart Contract Successfully Deployed', 'success');
+        this.contractDeployedSubject.next(true);
       },
       error => {
         this.coreService.isWorkingSubject.next(false);
